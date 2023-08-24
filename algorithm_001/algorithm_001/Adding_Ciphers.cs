8ef2fb0c18a7d33c645dedbfe6905d9da2d0f2c5
GYALLERHORN <kjins1998@gmail.com>
2023-08-24 11:52:01 +0900

    알고리즘_1
    (1~3번까지)

---
+++ algorithm_001/algorithm_001/Adding_Ciphers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm_001
+{
+    internal class Adding_Ciphers
+    {
+        //123을 각 자릿수 숫자를 다 더한 값 : 1 + 2 + 3 = 6 어떻게 하지?
+        public class Solution
+        {
+            public int solution(int n)
+            {
+                char[] ciphers = n.ToString().ToCharArray();
+                // ToArray는 기존의 배열(컬랙션)을 새로운 배열로 복사한다.
+                // 그래서 ToString()으로 만든 문자열을 배열화하는 방법으로는 ToCharArray()가 맞다.
+                int answer = 0;
+                for (int i = 0; i < ciphers.Length; i++)
+                {
+                    int a = int.Parse(ciphers[i].ToString());
+                    answer += a;
+                }
+                return answer;
+            }
+        }
+    }
+}
---
+++ algorithm_001/algorithm_001/Average.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithm_001
+{
+    internal class Average
+    {
+        public class Solution
+        {
+            public double solution(int[] arr)
+            {
+                double answer = 0;
+                double total = 0; // arr의 모든 원소를 다 더할 값 total
+                for (int i = 0; i < arr.Length; i++) // arr 길이만큼 반복하며
+                {
+                    total += arr[i]; // total에 arr의 첫번째 원소부터 다 더하고
+                }
+                answer = total / arr.Length; // 다 더한 값을 arr길이만큼 나눈다.
+                return answer;
+            } // ?? 이게 아무래도 최선인데?
+             // 아니 배열형 변수 내장 메서드에 Average()가 있었네..ㅡㅡ
+        }
+    }
+}
