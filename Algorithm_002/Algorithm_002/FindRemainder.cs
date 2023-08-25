9933ffaa3c2f432bf2d1b2d52b4db42f14394611
GYALLERHORN <kjins1998@gmail.com>
2023-08-25 10:34:56 +0900

    나머지가 1이 되는 수 찾기

---
+++ Algorithm_002/Algorithm_002/FindRemainder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_002
+{
+    internal class FindRemainder
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+            for (int i = n - 1; i > 1; i--)
+            {
+                if (n % i == 1)
+                {
+                    answer = i;
+                }
+            }
+            return answer;
+        }
+    }
+}
--- Algorithm_002/Algorithm_002/Operate.cs
+++ Algorithm_002/Algorithm_002/Operate.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Algorithm_002
+{
+    public class Operate
+    {
+        static void Main(string[] args)
+        {
+            FindRemainder findRemainder = new FindRemainder();
+            Console.WriteLine(" Operating Code");
+
+            int a = findRemainder.solution(7);
+            int b = findRemainder.solution(344);
+            int c = findRemainder.solution(5712);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
