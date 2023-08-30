9ed3aca843776bcc64b05f36cd5d0082ff2a54c9
GYALLERHORN <kjins1998@gmail.com>
2023-08-30 10:13:52 +0900

    콜라츠 추측 완료

---
+++ Algorithm_005/Algorithm_005/Collatz_supposition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_005
+{
+    public class Solution
+    {
+        public long solution(long num)
+        {
+            long answer = 0;
+            while (true)
+            {
+                if (num == 1)
+                {
+                    break;
+                }
+
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                }
+                else
+                {
+                    num = num * 3 + 1;
+                }
+                answer++;
+                if (answer >= 500)
+                {
+                    answer = -1;
+                    break;
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ Algorithm_005/Algorithm_005/Operator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_005
+{
+    public class Operator
+    {
+        static void Main()
+        {
+            Solution s = new Solution();
+            long a = s.solution(626331);
+            Console.WriteLine(a);
+        }
+    }
+}
