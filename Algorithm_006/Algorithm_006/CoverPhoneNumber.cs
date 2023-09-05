519067b8c136dd346fbe60db59fcdf245d2a417c
GYALLERHORN <kjins1998@gmail.com>
2023-09-05 10:03:16 +0900

    ~20 가운데 숫자 찾기 완료

--- Algorithm_006/Algorithm_006/CoverPhoneNumber.cs
+++ Algorithm_006/Algorithm_006/CoverPhoneNumber.cs
@@ -13,17 +13,9 @@ namespace Algorithm_006
         {
             string answer = "";
 
-            for (int i = 0; i < phone_number.Length; i++)
-            {
-                if (i < phone_number.Length - 4)
-                {
-                    answer += "*";
-                }
-                else
-                {
-                answer += phone_number[i];
-                }
-            }
+            answer = phone_number.Substring(phone_number.Length - 4); // 일단 answer에 전화번호의 뒷 4자리만 할당한다.
+            answer = answer.PadLeft(phone_number.Length, '*'); // answer의 문자열 길이를 전화번호 길이만큼 자리를 두고 빈 인덱스에는 *를 넣는다.
+
             return answer;
         }
     }
---
+++ Algorithm_007/Algorithm_007/_020_BringMiddleWord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_007
+{
+    public class _020_BringMiddleWord
+    {
+        public string solution(string s)
+        {
+            string answer = "";
+
+
+            if (s.Length % 2 == 1)
+            {
+                answer = s[s.Length / 2].ToString();
+            }
+            else
+            {
+                answer = s[s.Length / 2 - 1].ToString();
+                answer += s[s.Length / 2].ToString(); 
+            }
+
+            return answer;
+        }
+    }
+}
