a1af58b82c6b90c8e32f37c6b4686913b42b51d8
GYALLERHORN <kjins1998@gmail.com>
2023-09-07 11:57:24 +0900

    23 접근 제한자 수정

--- Algorithm_008/Algorithm_008/_023_CountofAliquotAndSum.cs
+++ Algorithm_008/Algorithm_008/_023_CountofAliquotAndSum.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Algorithm_008
 {
-    internal class CountofAliquotAndSum
+    public class CountofAliquotAndSum
     {
+        public int solution(int left, int right)
+        {
+            int answer = 0;
+
+            for (int i = left; i <= right; i++)
+            {
+                int aliquotCount = 0;
+                if ((float)Math.Sqrt(i) % 1f == 0f)
+                {
+                    aliquotCount++;
+                }
+                for (int j = 1; j < Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        aliquotCount += 2;
+                    }
+                }
+                if (aliquotCount % 2 == 0)
+                {
+                    answer += i;
+                }
+                else
+                {
+                    answer -= i;
+                }
+            }
+            return answer;
+        }
     }
 }
