d50bb9d1a2c40bc59a3ed55f05383ee44f92cf77
GYALLERHORN <kjins1998@gmail.com>
2023-09-08 10:20:53 +0900

    ~24 문자열 내림차순으로 배치하기 완료
    무려 LINQ를 사용했다.

---
+++ Algorithm_009/Algorithm_009/DescendingString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_009
+{
+    public class DescendingString
+    {
+        public string solution(string s) // 그냥 문자 하나하나를 char로 따고 그걸 ASCII 10진법으로 바꾸면..?
+        {
+            List<char> sArray = new List<char>();
+
+            foreach (char c in s)
+            {
+                sArray.Add(c);
+            }
+            var arr = from c in sArray
+                      orderby c 
+
+
+
+
+            string answer = "";
+            return answer;
+        }
+    }
+}
