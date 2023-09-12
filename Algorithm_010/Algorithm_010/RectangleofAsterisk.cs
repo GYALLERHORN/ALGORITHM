50de380835d1e275861a1c5bbb915b3f93476a89
GYALLERHORN <kjins1998@gmail.com>
2023-09-12 09:40:12 +0900

    ~28 직사각형 별찍기 완료

---
+++ Algorithm_010/Algorithm_010/RectangleofAsterisk.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_010
+{
+    public class Operator
+    {
+        public static void Main()
+        {
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+            int b = Int32.Parse(s[1]);
+
+            for (int i = 0; i < b; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
