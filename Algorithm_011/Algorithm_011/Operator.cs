227275bdc53ee403137c19588838f11eead7de63
GYALLERHORN <kjins1998@gmail.com>
2023-09-13 16:24:15 +0900

    3진수 문제 작성 중

---
+++ Algorithm_011/Algorithm_011/Operator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_011
+{
+    public class Operator
+    {
+        public static void Main(string[] args)
+        {
+            ReverseTernaryScale reverse = new ReverseTernaryScale();
+
+            Console.WriteLine(reverse.solution(12));
+        }
+    }
+}
---
+++ Algorithm_011/Algorithm_011/ReverseTernaryScale.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_011
+{
+    public class ReverseTernaryScale
+    {
+        public int solution(int n)
+        {
+            StringBuilder ternaryStr = new StringBuilder();
+
+            while (true)
+            {
+                ternaryStr.Append(n%3);
+                n = n / 3;
+                if (n < 3)
+                {
+                    ternaryStr.Append(n);
+                    break;
+                }
+            }
+
+            int answer = 0;
+
+            string sbToStr = ternaryStr.ToString();
+
+            //for (int i = 0; i <= sbToStr.Length -1; i++)
+            //{
+            //    answer += 1 * 3 ^ i * sbToStr[i];
+            //}
+            return answer;
+        }
+    }
+}
