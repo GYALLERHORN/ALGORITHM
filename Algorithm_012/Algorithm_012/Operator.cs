ea8afdfff9a4cc2415b55a800da711161280c83c
GYALLERHORN <kjins1998@gmail.com>
2023-09-15 10:02:17 +0900

    31 이상한 문자열 만들기 완료
    허접♥ 무슨 말 하는지도 모르는 (나쁜말)♡

---
+++ Algorithm_012/Algorithm_012/Operator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Algorithm_012
+{
+    public class Operator
+    {
+        static void Main(string[] args)
+        {
+            WeirdString weird = new WeirdString();
+            Console.WriteLine(weird.solution(" What do     you mean"));
+        }
+    }
+}
---
+++ Algorithm_012/Algorithm_012/WeirdString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_012
+{
+    public class WeirdString
+    {
+        public string solution(string s)
+        {
+            string answer = string.Empty;
+            for (int i = 0; i < s.Length; i++)
+            {
+                answer += i % 2 == 0 ? char.ToUpper(s[i]) : char.ToLower(s[i]);
+            }
+            return answer;
+        }
+    }
+}
