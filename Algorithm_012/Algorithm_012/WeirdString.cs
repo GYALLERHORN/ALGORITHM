e0e89fc660b9b5338b76ca5e0fcf93c91a9a76e2
GYALLERHORN <kjins1998@gmail.com>
2023-09-18 10:05:03 +0900

    Update WeirdString.cs

--- Algorithm_012/Algorithm_012/WeirdString.cs
+++ Algorithm_012/Algorithm_012/WeirdString.cs
@@ -1,19 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Algorithm_012
+﻿namespace Algorithm_012
 {
     public class WeirdString
     {
-        public string solution(string s)
+        public string solution(string s) // 문제 이해하는데 한참걸렸네 (나쁜말)
         {
             string answer = string.Empty;
-            for (int i = 0; i < s.Length; i++)
+
+            string[] splitArr = s.Split(' ');
+
+            for (int i = 0; i < splitArr.Length; i++)
             {
-                answer += i % 2 == 0 ? char.ToUpper(s[i]) : char.ToLower(s[i]);
+                for (int j = 0; j < splitArr[i].Length; j++)
+                {
+                    answer += j % 2 == 0 ? char.ToUpper(splitArr[i][j]) : char.ToLower(splitArr[i][j]);
+                }
+                if (i < splitArr.Length - 2)
+                {
+                    answer += " ";
+                }
             }
             return answer;
         }
