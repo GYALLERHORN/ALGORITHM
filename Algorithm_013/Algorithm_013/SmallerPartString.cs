83241921914a7e6316332435b259145895166b46
GYALLERHORN <kjins1998@gmail.com>
2023-09-18 11:01:41 +0900

    33 크기가 작은 문자열부분 완료

---
+++ Algorithm_013/Algorithm_013/SmallerPartString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_013
+{
+    public class SmallerPartString
+    {
+        public int solution(string t, string p)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < t.Length - (p.Length - 1); i++)
+            {
+                string str = t.Substring(i, p.Length);
+                answer += long.Parse(str) <= long.Parse(p) ? 1 : 0;
+            }
+            return answer;
+        }
+    }
+}
