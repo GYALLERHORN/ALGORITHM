11a336ff1de6487fe992ec022ed85f2d1eefef18
GYALLERHORN <kjins1998@gmail.com>
2023-09-18 11:03:33 +0900

    ~33 주석, 수정

--- Algorithm_013/Algorithm_013/SmallerPartString.cs
+++ Algorithm_013/Algorithm_013/SmallerPartString.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Algorithm_013
+﻿namespace Algorithm_013
 {
     public class SmallerPartString
     {
@@ -12,7 +6,7 @@ namespace Algorithm_013
         {
             int answer = 0;
 
-            for (int i = 0; i < t.Length - (p.Length - 1); i++)
+            for (int i = 0; i < t.Length - (p.Length - 1); i++) // p의 길이를 감안해서 t에서 뽑아낼 문자열 개수를 이렇게 계산했다.
             {
                 string str = t.Substring(i, p.Length);
                 answer += long.Parse(str) <= long.Parse(p) ? 1 : 0;
--- Algorithm_013/Algorithm_013/Trio.cs
+++ Algorithm_013/Algorithm_013/Trio.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Algorithm_013
+﻿namespace Algorithm_013
 {
     public class Trio
     {
