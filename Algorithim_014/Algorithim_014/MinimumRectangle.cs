83862098ef6aed921d955e2803ecd736442fe658
GYALLERHORN <kjins1998@gmail.com>
2023-09-19 10:29:34 +0900

    34 최소 직사작형 완료
    아 괜히 어렵게 생각해서 시간 다 잡아먹었네

---
+++ Algorithim_014/Algorithim_014/MinimumRectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithim_014
+{
+    public class MinimumRectangle
+    {
+        public int solution(int[,] sizes) // int[Width, Height]라고 합시다
+        {
+            int answer = 0;
+
+            int temp = 0;
+
+            for (int i = 0; i < sizes.GetLength(0); i++) // 배열을 돌면서
+            {
+                if (sizes[i, 0] < sizes[i, 1]) // Width < Height면 자리 바꾸기
+                {
+                    temp = sizes[i, 0];
+                    sizes[i, 0] = sizes[i, 1];
+                    sizes[i, 1] = temp;
+                }
+            } // 이 과정으로 전부 Width >= Height가 됩니다.
+
+            int maxWidth = sizes[0,0];
+            int maxHeight = sizes[0,1];
+
+            for (int i = 1; i < sizes.GetLength(0); i++)
+            {
+                maxWidth = sizes[i, 0] > maxWidth ? sizes[i,0] : maxWidth;
+            }
+            for (int i = 1; i < sizes.GetLength(0); i++)
+            {
+                maxHeight = sizes[i, 1] > maxHeight ? sizes[i, 1] : maxHeight;
+            }
+            answer = maxWidth * maxHeight;
+
+            return answer;
+        }
+    }
+}
