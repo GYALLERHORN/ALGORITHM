ebb20d87792d74da60b16e99e11abb4aeb2463cb
GYALLERHORN <kjins1998@gmail.com>
2023-09-21 09:52:50 +0900

    주석 추가

--- Algorithm_015/Algorithm_015/StringWihNUmberandWord.cs
+++ Algorithm_015/Algorithm_015/StringWihNUmberandWord.cs
@@ -9,9 +9,9 @@
             string[] wordArr = new string[10] { "zero", "one", "two","three", "four", "five", 
                 "six", "seven", "eight", "nine" };
 
-            for (int i = 0; i < wordArr.GetLength(0); i++)
+            for (int i = 0; i < wordArr.GetLength(0); i++) // 0~9 반복
             {
-                s = s.Replace(wordArr[i], i.ToString());
+                s = s.Replace(wordArr[i], i.ToString()); // 영단어가 있으면 교체
             }
             answer = int.Parse(s);
             return answer;
