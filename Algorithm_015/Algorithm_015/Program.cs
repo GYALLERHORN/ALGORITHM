752f9d58631562d7477bd37922924e3a624a3a56
GYALLERHORN <kjins1998@gmail.com>
2023-09-21 09:41:16 +0900

    36 숫자 문자열과 영단어 완료

---
+++ Algorithm_015/Algorithm_015/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Algorithm_015
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StringWihNUmberandWord word = new StringWihNUmberandWord();
+            string s = "one1four64sevennine";
+
+            string s2 = s.Replace("zero", "0");
+
+            int s1 = word.solution(s);
+
+            Console.WriteLine(s1);
+        }
+    }
+}
---
+++ Algorithm_015/Algorithm_015/StringWihNUmberandWord.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithm_015
+{
+    public class StringWihNUmberandWord
+    {
+        public int solution(string s)
+        {
+            int answer = 0;
+
+            string[] wordArr = new string[10] { "zero", "one", "two","three", "four", "five", 
+                "six", "seven", "eight", "nine" };
+
+            for (int i = 0; i < wordArr.GetLength(0); i++)
+            {
+                s = s.Replace(wordArr[i], i.ToString());
+            }
+            answer = int.Parse(s);
+            return answer;
+        }
+    }
+}
