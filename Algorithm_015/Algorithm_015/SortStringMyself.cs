1a06d2643dbaae97c2b8d8006fe5503024a82ceb
GYALLERHORN <kjins1998@gmail.com>
2023-09-22 10:02:36 +0900

    Update SortStringMyself.cs

--- Algorithm_015/Algorithm_015/SortStringMyself.cs
+++ Algorithm_015/Algorithm_015/SortStringMyself.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithm_015
 {
@@ -13,7 +11,8 @@ namespace Algorithm_015
             string[] answer = new string[] { };
 
             answer = strings.OrderBy(item => item[n]).ThenBy(item => item).ToArray();
-
+            // OrderBy한 틀 안에서 ThenBy(새로운 기준으로 다시한번 오름차순 정렬)한다.
+            // OrderBy([정렬 대상] => [정렬 방법(기준?)])
             return answer;
         }
     }
