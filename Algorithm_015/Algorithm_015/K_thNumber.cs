1a9aeaff3e9125686811fa6a39a0ddebb3e9013d
GYALLERHORN <kjins1998@gmail.com>
2023-09-24 18:16:08 +0900

    38 k번째수 정렬 완료

---
+++ Algorithm_015/Algorithm_015/K_thNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_015
+{
+    public class K_thNumber
+    {
+        public int[] solution(int[] array, int[,] commands)
+        {
+            List<int> arr = new List<int>();
+            
+            int[] tempArr = new int[] { };
+
+            int k = 0;
+
+            for (int count = 0; count < commands.GetLength(0); count++)
+            {
+                int tempArrLgth = commands[count, 1] - commands[count, 0] + 1;
+                tempArr = new int[tempArrLgth];
+                Array.Copy(array, commands[count, 0] - 1, tempArr, 0, tempArrLgth);
+
+                tempArr = tempArr.OrderBy(x => x).ToArray();
+
+                k = tempArr[commands[count, 2] - 1];
+
+                arr.Add(k);
+            }
+            int[] answer = arr.ToArray();
+            return answer;
+        }
+    }
+}
