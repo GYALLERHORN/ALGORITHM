0c237d8e0d4913abf87dfe796e427f09b1c20db3
GYALLERHORN <kjins1998@gmail.com>
2023-09-28 12:20:28 +0900

    42 콜라 문제 완료
    그냥 바로 암산 때려버리기

---
+++ Algorithm_016/Algorithm_016/ColaProblem.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithm_016
+{
+    internal class ColaProblem
+    {
+        public int solution(int a, int b, int n)
+        {
+            int answer = 0;
+            int temp;
+            while (n >= a)
+            {
+                temp = n / a * b;
+                answer += temp;
+                n = temp + n % a;
+            }
+            return answer;
+        }
+    }
+}
