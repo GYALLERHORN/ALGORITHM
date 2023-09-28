f9fd14dbc39d0a07236eef1480d27ec823c38c4a
GYALLERHORN <kjins1998@gmail.com>
2023-09-28 13:23:07 +0900

    44 2016년 완료
    편리한 DateTime

---
+++ Algorithm_016/Algorithm_016/_2016.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Algorithm_016
+{
+    internal class _2016
+    {
+        public string solution(int a, int b)
+        {
+            DateTime dateTime = new DateTime(2016, a, b);
+            string answer = "";
+            switch (dateTime.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    answer = "SUN";
+                    break;
+                    case DayOfWeek.Monday:
+                    answer = "MON";
+                    break;
+                    case DayOfWeek.Tuesday:
+                    answer = "TUE";
+                    break;
+                    case DayOfWeek.Wednesday:
+                    answer = "WED";
+                    break;
+                    case DayOfWeek.Thursday:
+                    answer = "THU";
+                    break;
+                    case DayOfWeek.Friday:
+                    answer = "FRI";
+                    break;
+                    case DayOfWeek.Saturday:
+                    answer = "SAT";
+                    break;
+            }
+            return answer;
+        }
+    }
+}
