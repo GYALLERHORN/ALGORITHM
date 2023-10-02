8c2722e9759d0ea97d4352bd0ea3c5b8a09914a3
GYALLERHORN <kjins1998@gmail.com>
2023-10-02 11:28:41 +0900

    45 카드 뭉치 완료

---
+++ Algorithm_016/Algorithm_016/CardBundle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_016
+{
+    internal class CardBundle
+    {
+        public string solution(string[] cards1, string[] cards2, string[] goal)
+        {
+            string answer = "Yes";
+
+            // 각 배열의 요소를 순서대로 비교하고, 하나라도 다른 순간 바로 No반환
+
+            int cards1card = 0;
+            int cards2card = 0;
+
+            for (int i = 0; i < goal.Length; i++)
+            {
+                if (cards1card < cards1.Length && cards1[cards1card] == goal[i])
+                {
+                    cards1card++;
+                    continue;
+                }
+                else if (cards2card < cards2.Length && cards2[cards1card] == goal[i])
+                {
+                    cards2card++;
+                    continue;
+                }
+                else
+                {
+                    return "No";
+                }
+            }
+
+            return answer;
+        }
+    }
+}
--- Algorithm_016/Algorithm_016/Program.cs
+++ Algorithm_016/Algorithm_016/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_016
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CardBundle cardBundle = new CardBundle();
+            string[] card1 = new string[] { "i", "water", "drink" };
+            string[] card2 = new string[] { "want", "to" };
+            string[] goal = new string[] { "i", "want", "to", "drink", "water" };
+
+            string answer = cardBundle.solution(card1, card2, goal);
+            Console.WriteLine(answer);
+        }
+    }
+}
