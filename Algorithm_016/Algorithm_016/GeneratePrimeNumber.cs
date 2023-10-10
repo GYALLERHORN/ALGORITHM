125fb03a769a99e50f374c5eb160f0c7a687df08
GYALLERHORN <kjins1998@gmail.com>
2023-10-10 10:01:18 +0900

    48 소수 만들기 완료

---
+++ Algorithm_016/Algorithm_016/GeneratePrimeNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_016
+{
+    internal class GeneratePrimeNumber
+    {
+        static void Main()
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            int answer = solution(nums);
+        }
+        static int solution(int[] nums)
+        {
+            int answer = 0;
+            int generatedNum;
+
+            for (int i = 0; i < nums.Length - 2; i++)
+                for (int j = i + 1; j < nums.Length - 1; j++)
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        generatedNum = i + j + k;
+                        for (int aliquot = 2; aliquot <= generatedNum; aliquot++)
+                        {
+                            if (generatedNum % aliquot == 0 && aliquot != generatedNum)
+                            {
+                                break;
+                            }
+                            if (aliquot == generatedNum)
+                            {
+                                answer++;
+                            }
+                        }
+                    }
+
+            return answer;
+        }
+    }
+}
--- Algorithm_016/Algorithm_016/Program.cs
+++ Algorithm_016/Algorithm_016/Program.cs
@@ -8,15 +8,15 @@ namespace Algorithm_016
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-            CardBundle cardBundle = new CardBundle();
-            string[] card1 = new string[] { "i", "water", "drink" };
-            string[] card2 = new string[] { "want", "to" };
-            string[] goal = new string[] { "i", "want", "to", "drink", "water" };
+        //static void Main(string[] args)
+        //{
+        //    CardBundle cardBundle = new CardBundle();
+        //    string[] card1 = new string[] { "i", "water", "drink" };
+        //    string[] card2 = new string[] { "want", "to" };
+        //    string[] goal = new string[] { "i", "want", "to", "drink", "water" };
 
-            string answer = cardBundle.solution(card1, card2, goal);
-            Console.WriteLine(answer);
-        }
+        //    string answer = cardBundle.solution(card1, card2, goal);
+        //    Console.WriteLine(answer);
+        //}
     }
 }
