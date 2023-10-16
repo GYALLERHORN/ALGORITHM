c039d2430dc3b7ac69ffaeb87a2a8e82a04022b5
GYALLERHORN <kjins1998@gmail.com>
2023-10-16 10:14:51 +0900

    49 덧칠하기 완료
    0으로 시작하는 변수의 의미를 다시 생각하는 코드다.

---
+++ Algorithm_016/Algorithm_016/PaintOver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_016
+{
+    internal class PaintOver
+    {
+        public int solution(int n, int m, int[] section)
+        {
+            int answer = 0;
+
+            int paintPosition = 0; // 0부터 시작하는 것에 대한 의미를 다르게 받아들여야 한다.
+            for (int i = 0; i < section.Length; i++)
+            {
+                if (paintPosition < section[i])
+                {
+                    paintPosition = section[i] + (m - 1);
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
