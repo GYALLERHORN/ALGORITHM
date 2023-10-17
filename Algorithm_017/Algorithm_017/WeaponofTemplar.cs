3151ce82e4b257d256cb36e929489efef2f0b3d1
GYALLERHORN <kjins1998@gmail.com>
2023-10-17 09:54:10 +0900

    50 기사단원의 무기 완료

---
+++ Algorithm_017/Algorithm_017/WeaponofTemplar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_017
+{
+    internal class WeaponofTemplar
+    {
+        public int solution(int number, int limit, int power) // 참고 : 23_약수의 개수와 덧셈
+        {
+            int answer = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                int aliquotCount = 0;
+
+                if ((float)Math.Sqrt(i) % 1 == 0)
+                {
+                    aliquotCount++;
+                }
+
+                for (int j = 1; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        aliquotCount += 2;
+                    }
+                }
+
+                if (aliquotCount > limit)
+                {
+                    aliquotCount = power;
+                }
+
+                answer += aliquotCount;
+            }
+
+            return answer;
+        }
+    }
+}
