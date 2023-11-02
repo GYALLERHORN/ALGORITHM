ce6d126d1b17436e36c30c7f567d1cdbdc6965b5
GYALLERHORN <kjins1998@gmail.com>
2023-11-02 15:57:56 +0900

    52 옹알이(2) 주석 추가

--- Algorithm_017/Algorithm_017/Babbling.cs
+++ Algorithm_017/Algorithm_017/Babbling.cs
@@ -8,32 +8,32 @@ namespace Algorithm_017
         {
             int answer = 0;
 
-            string[] pronuns = new string[4] { "aya", "ye", "woo", "ma" };
+            string[] pronuns = new string[4] { "aya", "ye", "woo", "ma" }; // 옹알이 가능한 발음
 
-            for (int bab = 0; bab < babbling.Length; bab++)
+            for (int bab = 0; bab < babbling.Length; bab++) // 확인 대상 단어 한개씩 순회
             {
                 for (int i = 0; i < pronuns.Length; i++)
                 {
-                    string cantSpeak = pronuns[i] + pronuns[i];
+                    string cantSpeak = pronuns[i] + pronuns[i]; 
 
-                    if (babbling[bab].Contains(cantSpeak))
+                    if (babbling[bab].Contains(cantSpeak)) // 옹알이 발음이 연속된 단어가 2개 연속된 경우
                     {
-                        babbling[bab] = "X";
+                        babbling[bab] = "X"; // 대상에서 제외 => 값을 X로 전환
                         continue;
                     }
 
-                    babbling[bab] = babbling[bab].Replace(pronuns[i], "O");
+                    babbling[bab] = babbling[bab].Replace(pronuns[i], "O"); // 발음 가능한 부분이 있는 경우, 그 부분을 O로 전환
                 }
 
-                foreach (char c in babbling[bab])
+                foreach (char c in babbling[bab]) // 대상 언어를 알파벳 한개씩 나눠서 순회
                 {
-                    if (c != 'O')
+                    if (c != 'O') // 알파벳 중 O이 아닌 알파벳이 있을 경우(발음 가능한 단어 외의 요소가 있는 경우)
                     {
-                        babbling[bab] = "X";
+                        babbling[bab] = "X"; // 대상에서 제외 => 값을 X로 전환
                         continue;
                     }
                 }
-                if (babbling[bab].Contains("O")) answer++;
+                if (babbling[bab].Contains("O")) answer++; // 위 과정을 거치면 순수하게 O만 포함한 단어를 찾을 수 있다.
             }
             return answer;
         }
